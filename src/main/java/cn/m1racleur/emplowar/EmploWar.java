75cd2b10008b36b86e5b69179259a04a3fa19c1a
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-01-18 00:28:07 +0800

    init

---
+++ src/main/java/cn/m1racleur/emplowar/EmploWar.java
@@ -0,0 +1,17 @@
+package cn.m1racleur.emplowar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EmploWar extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
