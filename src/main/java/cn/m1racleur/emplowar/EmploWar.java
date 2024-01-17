0577b46ac8491bc72c0d84c8a7e6427fb8432a48
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-01-18 00:30:24 +0800

    init

--- src/main/java/cn/m1racleur/emplowar/EmploWar.java
+++ src/main/java/cn/m1racleur/emplowar/EmploWar.java
@@ -6,12 +6,9 @@ public final class EmploWar extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
