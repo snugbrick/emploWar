9437ab52e5121e5b1ccfeaa849c432cab5d110f6
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-01-23 04:11:56 +0800

    ?

--- src/main/java/cn/m1racleur/emplowar/api/emplo/Emplos.kt
+++ src/main/java/cn/m1racleur/emplowar/api/emplo/Emplos.kt
@@ -2,15 +2,21 @@ package cn.m1racleur.emplowar.api.emplo
 
 import org.bukkit.inventory.ItemStack
 
-/**
-@version 1.0.0 2024.01.22 14:54
-@author MiracleUR -> github.com/snugbrick
- */
+/** @version 1.0.0 2024.01.22 14:54 @author MiracleUR -> github.com/snugbrick */
 interface Emplos {
     val name: String?
 
     val lore: String?
 
-    val deffKnapsack: Array<ItemStack>  //背包
+    val deffKnapsack: Array<ItemStack> // 背包
 
+    fun getName(): String? {
+        return name
+    }
+    fun getLore(): String? {
+        return lore
+    }
+    fun getdDeffKnapsack(): Array<ItemStack> {
+        return deffKnapsack
+    }
 }
